ad069afd6f8d13b29c34f5752b3287ceccbcba00
michela iacobucci <miacobucci17@gmail.com>
2021-05-04 16:58:28 -0700

    beginning tasks

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -4,4 +4,22 @@ pragma solidity ^0.5.0;
 
 contract ToDoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out my blockchain");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++; 
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
